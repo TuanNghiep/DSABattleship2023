be79ebfc4ee3d64c72785f121a0ebae40d7655ef
ThaiKhang123456 <134149326+ThaiKhang123456@users.noreply.github.com>
2023-12-29 21:09:35 +0700

    Update FrameSplashscreen.java

--- main/ui/FrameSplashscreen.java
+++ main/ui/FrameSplashscreen.java
@@ -1,41 +1,32 @@
+package battleship_main.ui;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class FrameSplashscreen extends JFrame {
+public class FrameSplashscreen extends JFrame{
+    private static final long serialVersionUID = 1L;
 
     public FrameSplashscreen() {
-        setTitle("Battleship Splashscreen");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Create and add components to the frame, e.g., JLabel for the splash screen
-        JLabel splashLabel = new JLabel("Battleship Game Splash Screen");
-        splashLabel.setFont(new Font("Arial", Font.BOLD, 20));
-        splashLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-        add(splashLabel);
-
-        // Set layout, size, and other frame properties
-        setLayout(new BorderLayout());
-        setSize(400, 200);
-        setLocationRelativeTo(null); // Center the frame
-
-        // Use Timer to close the splash screen after a delay (e.g., 3 seconds)
-        Timer timer = new Timer(3000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose(); // Close the splash screen
-                new FrameManageOctopus(); // Open the main frame
-            }
-        });
-        timer.setRepeats(false); // Only execute once
-        timer.start();
-
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new FrameSplashscreen());
+        super("Battleship");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setUndecorated(true);
+        this.setSize(600, 350);
+        this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/res/images/icon.png")));
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+        this.setLocation(x, y);
+        JPanel container = new JPanel(null);
+        UIJPanelBG splashPanel = new UIJPanelBG(
+                Toolkit.getDefaultToolkit().createImage(getClass().getResource("/res/images/splash.png")));
+        ImageIcon loadingIMG = new ImageIcon(getClass().getResource("/res/images/loading.gif"));
+        JLabel loadingLabel = new JLabel(loadingIMG);
+        container.add(splashPanel);
+        splashPanel.setBounds(0, 0, 600, 350);
+        container.add(loadingLabel, 0);
+        loadingLabel.setBounds(220, 250, 160, 40);
+        this.add(container);
+        this.setVisible(true);
     }
 }
